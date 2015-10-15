1efff05a70c21dec9c13376f47bb20a989b9605f
Sameer Chitley <sameer@yelp.com>
2015-10-15 00:21:25 -0700

    Refactored variable names

--- parser/lexer.py
+++ parser/lexer.py
@@ -6,24 +6,24 @@ from tokens import TokenType
 
 class Lexer:
 
-    json_string = None
-    peeked_token = None
-    position = None
+    _json_string = None
+    _peeked_token = None
+    _position = None
 
     def __init__(self, json_string):
-        self.json_string = json_string
-        self.position = 0
+        self._json_string = json_string
+        self._position = 0
 
     def _action_and_advance(self, func):
         token = func()
-        self.position += 1
-        self.peeked_token = None
+        self._position += 1
+        self._peeked_token = None
         return token
 
     def _advance_to_non_whitespace(self):
-        while (self.position < len(self.json_string)
-                and self.json_string[self.position: self.position+1].isspace()):
-            self.position += 1
+        while (self._position < len(self._json_string)
+                and self._json_string[self._position: self._position+1].isspace()):
+            self._position += 1
 
     def _try_parse_number(self):
         raise NotImplementedError
@@ -33,48 +33,48 @@ class Lexer:
         raise NotImplementedError
 
     def _try_parse_value(self, token):
-        if self.json_string[self.position:].startswith(token.token_value):
-            self.position += len(token.token_value)
+        if self._json_string[self._position:].startswith(token.token_value):
+            self._position += len(token.token_value)
             return token
         raise ParsingException(
             'Invalid token at position {}. Expected "{}".'.format(
-                self.position,
+                self._position,
                 token.token_value,
             )
         )
 
     def next(self):
-        peeked_token = self.peeked_token
+        peeked_token = self._peeked_token
         if peeked_token:
-            self.position += len(peeked_token)
-            self.peeked_token = None
+            self._position += len(peeked_token)
+            self._peeked_token = None
             return peeked_token
 
         self._advance_to_non_whitespace()
 
-        if self.position >= len(self.json_string):
-            return Token(TokenType.EOF, '', self.position)
+        if self._position >= len(self._json_string):
+            return Token(TokenType.EOF, '', self._position)
 
         token_action_mapper = {
-            't': lambda: self._try_parse_value(Token(TokenType.TRUE, 'true', self.position)),
-            'f': lambda: self._try_parse_value(Token(TokenType.FALSE, 'false', self.position)),
-            'n': lambda: self._try_parse_value(Token(TokenType.NULL, 'null', self.position)),
+            't': lambda: self._try_parse_value(Token(TokenType.TRUE, 'true', self._position)),
+            'f': lambda: self._try_parse_value(Token(TokenType.FALSE, 'false', self._position)),
+            'n': lambda: self._try_parse_value(Token(TokenType.NULL, 'null', self._position)),
             '"': lambda: self._try_parse_string(),
-            '{': lambda: Token(TokenType.START_OBJECT, '{', self.position),
-            '}': lambda: Token(TokenType.END_OBJECT, '}', self.position),
-            '[': lambda: Token(TokenType.START_ARRAY, '[', self.position),
-            ']': lambda: Token(TokenType.END_ARRAY, ']', self.position),
-            ':': lambda: Token(TokenType.COLON, ':', self.position),
-            ',': lambda: Token(TokenType.COMMA, ',', self.position),
+            '{': lambda: Token(TokenType.START_OBJECT, '{', self._position),
+            '}': lambda: Token(TokenType.END_OBJECT, '}', self._position),
+            '[': lambda: Token(TokenType.START_ARRAY, '[', self._position),
+            ']': lambda: Token(TokenType.END_ARRAY, ']', self._position),
+            ':': lambda: Token(TokenType.COLON, ':', self._position),
+            ',': lambda: Token(TokenType.COMMA, ',', self._position),
         }
 
-        token_action = token_action_mapper.get(self.json_string[self.position], self._try_parse_number)
+        token_action = token_action_mapper.get(self._json_string[self._position], self._try_parse_number)
         return self._action_and_advance(token_action)
 
     def peek(self):
-        if not self.peeked_token:
-            self.peeked_token = self.next()
-        return self.peeked_token
+        if not self._peeked_token:
+            self._peeked_token = self.next()
+        return self._peeked_token
 
 
 class ParsingException(Exception):
